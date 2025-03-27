08190e6dff563a2d6fdc1e37ca66010f2b01a2d7
fullstackbabulal <fullstack.babulal@gmail.com>
2025-03-27 16:02:42 +0530

    add onClick

--- Healthy-Food-v1/src/components/Items.jsx
+++ Healthy-Food-v1/src/components/Items.jsx
@@ -1,6 +1,8 @@
 import styles from "./Items.module.css"
 export let Items = ({FoodName}) => (
   <li className="list-group-item">{FoodName}
-  <button className={`${styles.button} btn btn-info`}>Buy</button>
+  <button className={`${styles.button} btn btn-info`}
+  onClick={()=>console.log(`${FoodName} being bought`)}
+  >Buy</button>
   </li>
 )
