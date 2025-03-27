be001ff62264aea92074742d341897e2fc53b8e1
fullstackbabulal <fullstack.babulal@gmail.com>
2025-03-27 13:36:26 +0530

    Passing a component as a child to Container

---
+++ Healthy-Food-v1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Container from './components/Container'
+import Heading from './components/Heading'
+function App() {
+
+  return (
+    <Container>
+    <Heading></Heading>
+    </Container>
+  );
+};
+
+export default App;
---
+++ Healthy-Food-v1/src/components/Heading.jsx
@@ -0,0 +1,6 @@
+import styles from './Heading.module.css';
+const Heading = () => (
+  <h1 className={styles.heading}>Healthy Food</h1>
+)
+
+export default Heading;
